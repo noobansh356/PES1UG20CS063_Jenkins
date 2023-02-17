6aac4fa1461eedb028d1012bf92eb8f23737b84f
Anshul Rao <82894424+noobansh356@users.noreply.github.com>
2023-02-17 10:58:27 +0530

    Update pes1ug20cs063-1.cpp

--- pes1ug20cs063-1.cpp
+++ pes1ug20cs063-1.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main(void) {
-  cout << "This is the new CPP file created by Anshul-Rao (PES1UG20CS063)\n";
-  return 0;
+  cout << "This is the new CPP file created by Anshul-Rao (pes1ug20cs063)\n";
+  return 0;
 }
